2fda781a3a1db0243b0abd7a09a1b163a2253cd7
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-05-17 16:58:13 +0200

    push

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+const content = document.querySelector(".content");
+const addBtn = document.querySelector(".add");
+
 let Ncasuali = [];
 
+addBtn.addEventListener("click", function(){
+
+    
+    while (Ncasuali.length < 5) {
+        let i = Math.floor(Math.random()*10)+1;
+        Ncasuali.push(i);
+    }  
+    console.log(Ncasuali);
+    const numelem = document.createElement("div");
+    content.append(numelem);
+    numelem.append(Ncasuali);
+
+    setTimeout(myFunction, 5000);
+
+});
 
 
-for (let i = 0; i < 10; i++) {
-
-    function Nrandomici() {
-        return Math.floor(Math.random()*10)+1;
-    }
+function myFunction(){
+    let risposta = prompt("Qali sono i numeri inseriti?")
+    console.log(risposta);
+    return risposta;
     
 }
 
-console.log(Ncasuali);
+
+
+
